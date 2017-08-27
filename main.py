49716105591caafeedcbf6a06e786709f8e72846
MedicKuS <medickus97@gmail.com>
2017-08-27 23:28:00 +0300

    create 1 level full

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Menu:
                 layout.blit(font.render(i[2], 1, i[3]), (i[0], i[1]))
     '''функция цикла сцены, которая вызывает меню'''
     def menu(self):
+        menu_font = font.Font('9921.otf', 50) # шрифт
         done = True
         item = 0
         while done:
@@ -37,6 +38,11 @@ class Menu:
                     if ev.key == K_DOWN:
                         if item < len(self.items) - 1:
                             item = item + 1
+                    if ev.key == K_SPACE:
+                        if item == 0:
+                            done = False
+                        elif item == 1:
+                            sys.exit()
                 if ev.type == MOUSEBUTTONDOWN and ev.button == 1:
                     if item == 0:
                         done = False
@@ -49,30 +55,45 @@ class Level1:
     def __init__(self, text = [0 , 0, 'text', (0, 0, 0), 0]):
         self.text = text
     def level1(self):
+        dialog_font = font.Font('9921.otf', 24) # шрифт
         init = True
+        boll = True
+        n = 0
         while init:
-            boll = True
             logo = image.load('image/intro/intro.png')
             dialog_hero = image.load('image/intro/dialog_ghero.png')
             dialog_phone = image.load('image/intro/dialog_phone.png')
+            if boll == True:
+                i = self.text[n]
+                dialog_phone.blit(dialog_font.render(i[2], 1, i[3]), (i[0], i[1]))
+                logo.blit(dialog_phone, (0, 500))
+                scene.blit(logo, (0, 0))
+                display.flip()
+            else:
+                i = self.text[n]
+                dialog_hero.blit(dialog_font.render(i[2], 1, i[3]), (i[0], i[1]))
+                logo.blit(dialog_hero, (0, 500))
+                scene.blit(logo, (0, 0))
+                display.flip()
             for ev in event.get():
                 if ev.type == QUIT:
                     init = False
-            for i in self.text:
-                if boll == True:
-                    dialog_phone.blit(menu_font.render(i[2], 1, i[3]), (i[0], i[1]))
-                    logo.blit(dialog_phone, (0, 500))
-                    boll = False
-                    scene.blit(logo, (0, 0))
-                    display.flip()
-                else:
-                    dialog_hero.blit(menu_font.render(i[2], 1, i[3]), (i[0], i[1]))
-                    logo.blit(dialog_hero, (0, 500))
-                    boll = True
-                    scene.blit(logo, (0, 0))
-                    display.flip()
-            scene.blit(logo, (0, 0))
-            display.flip()
+                if ev.type == KEYDOWN:
+                    if ev.key == K_ESCAPE:
+                        sys.exit()
+                    if ev.key == K_SPACE:
+                        if boll == True:
+                            boll = False
+                            if n < 12:
+                                n = n + 1
+                            else:
+                                gameMenu.menu()
+                        else:
+                            boll = True
+                            if n < 12:
+                                n = n + 1
+                            else:
+                                gameMenu.menu()
 
 '''главный блок программы'''
 scene = display.set_mode((1000, 673)) # Создание пустого рабочего окна
@@ -80,7 +101,6 @@ logo = image.load('image/logo.ico')
 display.set_icon(logo) #Логотип (иконка) игры
 display.set_caption('Omnipotient') # Название игры
 font.init() # иницализация шрифтов
-menu_font = font.Font('9921.otf', 50) # шрифт
 
 ''' список кнопок в меню (x, y, name, color_def, act_color, num) '''
 items = [
@@ -93,19 +113,21 @@ gameMenu.menu()
 
 '''инициализация 1 уровня (демо) *1 диалог'''
 text = [
-    (300, 5, 'звонок', (5, 5, 5), 0),
-    (300, 5, 'Да?', (5, 5, 5), 1),
-    (300, 5, 'Это я, у меня есть поручение для тебя.', (5, 5, 5), 2),
-    (300, 5, 'Я уже ожидаю худшего.', (5, 5, 5), 3),
-    (300, 5, 'Да потерпи ты. Наш уговор в силе, и тебе осталось совсем чуть-чуть. Нужно совершить сделку в отеле Монтана, с директором.', (5, 5, 5), 4),
-    (300, 5, 'Надеюсь, я сам буду?', (5, 5, 5), 5),
-    (300, 5, 'Нет, с тобой будет мой человек. Его нужно многому учить, но стреляет он неплохо.', (5, 5, 5), 6),
-    (300, 5, 'Блять...', (5, 5, 5), 7),
-    (300, 5, '...Наш клиент последнее время очень не доволен нашими условиями и, по слухам, плохо о нас высказывается.', (5, 5, 5), 8),
-    (300, 5, 'Если на вас будет покушение - убейте его и взорвите отель.', (5, 5, 5), 9),
-    (300, 5, 'Блять...', (5, 5, 5), 10),
-    (300, 5, 'Не ной. Задание простое. На месте напарник тебя уже будет ждать. Он знает, как ты выглядишь. Удачи.', (5, 5, 5), 11),
-    (300, 5, '...', (5, 5, 5), 12)
+    (300, 7, 'звонок', (5, 5, 5), 0),
+    (300, 7, 'Да?', (5, 5, 5), 1),
+    (300, 7, 'Это я, у меня есть поручение для тебя.', (5, 5, 5), 2),
+    (300, 7, 'Я уже ожидаю худшего.', (5, 5, 5), 3),
+    (300, 7, 'Да потерпи ты. Наш уговор в силе, и тебе осталось совсем чуть-чуть. Нужно совершить сделку в отеле Монтана, с директором.', (5, 5, 5), 4),
+    (300, 7, 'Надеюсь, я сам буду?', (5, 5, 5), 5),
+    (300, 7, 'Нет, с тобой будет мой человек. Его нужно многому учить, но стреляет он неплохо.', (5, 5, 5), 6),
+    (300, 7, 'Блять...', (5, 5, 5), 7),
+    (300, 7, '...Наш клиент последнее время очень'
+             ' не доволен нашими условиями и, по слухам, плохо о нас высказывается.', (5, 5, 5), 8),
+    (300, 7, 'Если на вас будет покушение - убейте его и взорвите отель.', (5, 5, 5), 9),
+    (300, 7, 'Блять...', (5, 5, 5), 10),
+    (300, 7, 'Не ной. Задание простое.'
+             ' На месте напарник тебя уже будет ждать. Он знает, как ты выглядишь. Удачи.', (5, 5, 5), 11),
+    (300, 7, '...', (5, 5, 5), 12)
 ]
 gameLevel1 = Level1(text)
 gameLevel1.level1()
